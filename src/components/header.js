64119e04ed6b822b731e5b494532bd304649255a
bushbass <alex.nielsen@gmail.com>
2019-05-24 15:39:43 -0400

    finished lesson 7, start at 1:28:00

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,54 @@
 import React from "react"
 import { Link } from "gatsby"
+import headerStyles from "./header.module.scss"
 
 function Header() {
   return (
-    <div>
-      <h1>Alex Nielsen's Gatsby Page</h1>
-      <ul style={{ listStyle: "none", display: "flex" }}>
-        <li>
-          <Link style={{ textDecoration: "none", padding: 5 }} to="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link style={{ textDecoration: "none", padding: 5 }} to="about">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link style={{ textDecoration: "none", padding: 5 }} to="contact">
-            Contact
-          </Link>
-        </li>
-        <li>
-          <Link style={{ textDecoration: "none", padding: 5 }} to="blog">
-            Blog
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <header className={headerStyles.header}>
+      <Link className={headerStyles.title} to="/">
+        <h1>Alex Nielsen's Gatsby Page</h1>
+      </Link>
+      <nav>
+        <ul className={headerStyles.navList}>
+          <li>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="about"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="contact"
+            >
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="blog"
+            >
+              Blog
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
